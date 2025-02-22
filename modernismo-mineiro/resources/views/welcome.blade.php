504dc42e14ed0adc6c91cfef67b9da01f59212ec
Lucas Galarani <Lucasgalaranidc@gmail.com>
2025-02-22 20:35:20 -0300

    arquivos base

---
+++ modernismo-mineiro/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modernismo Mineiro</title>
+    @vite('resources/js/app.js')
+</head>
+<body class="bg-gray-100 text-gray-900">
+    <h1 class="bg-blue-100 text-3xl font-bold text-center mt-10">Bem-vindo ao Modernismo Mineiro</h1>
+</body>
+</html>
